fd423b21bb7b7d93cfb34f0933388ec654358705
Cong Dat <congdat.hutech@gmail.com>
2023-03-24 15:40:41 +0700

    dat_24/03/2023

--- admin/index.php
+++ admin/index.php
@@ -11,7 +11,7 @@
 
 <div class="main_bot">
             <h1>Chào Mừng Bạn Đến Với Trang Admin</h1>
-                <img src="image/datquang.jpg" alt="Image description" width="400" height="500" style="display: block; margin: auto;">
+                <img src="image/datquang1.jpg" alt="Image description" width="600" height="500" style="display: block; margin: auto;">
 
         </div>
      
---
+++ admin/quanlymenu.php
@@ -0,0 +1,45 @@
+<?php
+include "menu.php";
+
+?> 
+
+<div class="main_add">
+            <h3>Thêm Menu</h3>
+            <form action="xulymenu.php" method="post">
+                <div class="add_them">
+                    <label for="ten_menu">Thêm Menu</label>
+                    <input type="text" name="ten_menu" placeholder="Nhập Tên Menu ... " required>
+
+                </div>
+                <button name="them" type="submit">Thêm</button>
+            </form>
+
+            <?php
+	            $sql_lietke_menu = "SELECT * FROM tbl_menu";
+	            $query_lietke_menu = mysqli_query($mysqli,$sql_lietke_menu);
+            ?>
+            <h3>Danh Sách</h3>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Menu</th>
+                    <th>Edit</th>
+                </tr>
+                <?php
+                    $i = 0;
+                    while($row = mysqli_fetch_array($query_lietke_menu)){
+                	$i++;
+                ?>
+                <tr>
+                    <td><?php echo $i ?></td>
+                    <td><?php echo $row['ten_menu'] ?></td>
+                    <td>
+                    <a href="">Sửa</a>
+                        <a href="">Xóa</a>
+                    </td>
+                </tr>
+                <?php
+                    }
+                ?>
+            </table>
+        </div>
