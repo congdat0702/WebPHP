9448aaa8d43e4da861501b56adeba2641ea8222e
Cong Dat <congdat.hutech@gmail.com>
2023-04-01 09:58:52 +0700

    dat_1/4/23

--- admin/menu.php
+++ admin/menu.php
@@ -27,6 +27,6 @@
                 <li><a href="index.php">Trang Chủ</a></li>
                 <li><a href="quanlymenu.php">Quản Lý Menu</a></li>
                 <li><a href="quanlysanpham.php">Quản Lý Sản Phẩm</a></li>
-                <li><a href="">Quản Lý Đơn Hàng</a></li>
+                <li><a href="donhang.php">Quản Lý Đơn Hàng</a></li>
             
             </div>
