d36de2fa72cfb0375511080b04eb17e73767d991
Cong Dat <congdat.hutech@gmail.com>
2023-04-02 18:51:10 +0700

    a

--- admin/config/config.php
+++ admin/config/config.php
@@ -1,5 +1,5 @@
 <?php 
-	$mysqli = new mysqli("localhost","root","root","thietbidientu");
+	$mysqli = new mysqli("localhost","root","root","webthietbidientu");
 	if ($mysqli->connect_errno) {
 	  echo "Kết nối MYSQLi lỗi" . $mysqli->connect_error;
 	  exit();
